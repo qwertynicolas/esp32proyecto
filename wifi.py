eb753514965741c660211f5fce323b25ff8551aa
qwertynicolas <qwertynicolas@proton.me>
2024-11-08 21:07:40 -0300

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,10 +1,10 @@
 def do_connect():
     import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
         print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect('<ssid>', '<key>') # cambiar estas variables por las que correspondan
-        while not sta_if.isconnected():
+        wlan.connect('ssid', 'key') # variables del wifi 
+        while not wlan.isconnected():
             pass
-    print('network config:', sta_if.ipconfig('addr4'))
+    print('network config:', wlan.ipconfig('addr4'))
